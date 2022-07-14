9702b3c10e5e033f46adf4ec3908c04f9af1fb30
DESKTOP-25OQ4IL\Kwicot <kwicot@gmail.com>
2022-07-14 20:19:51 +0300

    Remove procedural generation

--- Assets/_Core/Scripts/TownGeneration/TownGenerator.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TownGenerator : MonoBehaviour
-{
-    [SerializeField] private Terrain targetTerrain;
-    [SerializeField] private LineGenerator lineGenerator;
-}
