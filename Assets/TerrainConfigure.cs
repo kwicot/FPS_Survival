09635a50d0c36507af296679695a5ce5b4950f78
DESKTOP-25OQ4IL\Kwicot <kwicot@gmail.com>
2022-07-31 15:14:50 +0300

    Created Level

--- Assets/TerrainConfigure.cs
+++ Assets/TerrainConfigure.cs
@@ -1,27 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class TerrainConfigure : MonoBehaviour
 {
+    private bool reSize;
+    private bool reMove;
+    
     private void Start()
     {
-        StartCoroutine(Configure());
     }
 
-    private IEnumerator Configure()
-    {
-        for (int x = 0; x < 32; x++)
-        {
-            for (int y = 0; y < 32; y++)
-            {
-                var data = transform.GetChild(x + (y * 32));
-                var terrainData = data.GetComponent<Terrain>().terrainData;
 
-                terrainData.heightmapResolution = 641;
-                yield return null;
-            }
-        }
-    }
+   
 }
--- Assets/_Core/Scripts/MapImporter.cs
+++ Assets/_Core/Scripts/MapImporter.cs
@@ -12,6 +12,12 @@ namespace _Core.Scripts
 
         private float[,] heightMap;
 
+        private TerrainData[,] terrainsData;
+        private GameObject[,] terrains;
+        private int dataSize = 8;
+        int size = 1024;
+
+
         private void Start()
         {
             StartCoroutine(GenerateMap());
@@ -20,42 +26,18 @@ namespace _Core.Scripts
         IEnumerator GenerateMap()
         {
             yield return ReadTexture();
+            Debug.Log("Read texture done");
             yield return CreateTerrains();
-
-        }
-
-        IEnumerator CreateTerrains()
-        {
-            Debug.Log("Start create terrains");
-            int size = 256;
-            
-            for (int i = 0; i < 32; i++)
-            {
-                for (int j = 0; j < 32; j++)
-                {
-                    var map = new float[size, size];
-                    
-                    for (int x = 0; x < size; x++)
-                        for (int y = 0; y < size; y++)
-                            map[x, y] = heightMap[y + i * size, x + j * size];
-                    
-                    var terrainData = new TerrainData();
-                    terrainData.heightmapResolution = size + 1;
-                    terrainData.size = new Vector3(size, size / 4, size);
-                    terrainData.SetHeights(0,0,map);
-                    var terrain = Terrain.CreateTerrainGameObject(terrainData);
-                    terrain.transform.position = new Vector3(i * size, 100, j * size);
-                    
-                    AssetDatabase.CreateAsset(terrainData,$"Assets/TerrainData/Terrain_{i}_{j}.asset");
-                    
-                    yield return null;
-                }
-            }
-
             Debug.Log("Create terrains done");
+            yield return ReSize();
+            Debug.Log("Resize done");
+            yield return Move();
+            Debug.Log("Move done");
+            yield return SaveTerrains();
+            Debug.Log("Save done");
         }
-
-        IEnumerator ReadTexture()
+        
+        private IEnumerator ReadTexture()
         {
             Debug.Log("Start read texture");
             heightMap = new float[heightMapTexture.width,heightMapTexture.height];
@@ -79,5 +61,82 @@ namespace _Core.Scripts
             }
             Debug.Log("Read texture done");
         }
+        private IEnumerator CreateTerrains()
+        {
+            terrainsData = new TerrainData[dataSize,dataSize];
+            terrains = new GameObject[dataSize, dataSize];
+            Debug.Log("Start create terrains");
+            
+            for (int i = 0; i < dataSize; i++)
+            {
+                for (int j = 0; j < dataSize; j++)
+                {
+                    var map = new float[size, size];
+                    
+                    for (int x = 0; x < size; x++)
+                        for (int y = 0; y < size; y++)
+                            map[x, y] = heightMap[y + i * size, x + j * size];
+                    
+                    var terrainData = new TerrainData();
+                    terrainData.heightmapResolution = size+1;
+                    terrainData.size = new Vector3(size, size / 4, size);
+                    terrainData.SetHeights(0,0,map);
+                    var terrain = Terrain.CreateTerrainGameObject(terrainData);
+                    terrain.transform.position = new Vector3(i * size, 100, j * size);
+                    
+                    terrainsData[i, j] = terrainData;
+                    terrains[i, j] = terrain;
+                    
+                    yield return null;
+                }
+            }
+
+            Debug.Log("Create terrains done");
+        }
+        private IEnumerator ReSize()
+        {
+            for (int x = 0; x < dataSize; x++)
+            {
+                for (int y = 0; y < dataSize; y++)
+                {
+                    var terrainData = terrainsData[x, y];
+            
+                    terrainData.size = new Vector3(size * 4, size, size * 4);
+                    yield return null;
+                }
+            }
+
+            Debug.Log("End");
+        }
+        private IEnumerator Move()
+        {
+            for (int x = 0; x < dataSize; x++)
+            {
+                for (int y = 0; y < dataSize; y++)
+                {
+                    var data = terrains[x,y];
+                    var pos = data.transform.position;
+                    pos.x *= 4;
+                    pos.z *= 4;
+
+                    data.transform.position = pos;
+                    yield return null;
+                }
+            }
+        }
+        private IEnumerator SaveTerrains()
+        {
+            for (int x = 0; x < dataSize; x++)
+            {
+                for (int y = 0; y < dataSize; y++)
+                {
+                    AssetDatabase.CreateAsset(terrainsData[x,y],$"Assets/TerrainData/Terrain_{x}_{y}.asset");
+                    
+                }
+
+                yield return null;
+            }
+        }
+
     }
 }
